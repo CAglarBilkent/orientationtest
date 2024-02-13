bfb92103d67906c9eed5fcd1fb9cc1e0279069c7
Caglar Alim Aglar <alimaglar@gmail.com>
2024-02-12 21:37:52 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function App() {
+  const [orientation, setOrientation] = useState(1);
+
+  useEffect(() => {
+    ScreenOrientation.unlockAsync();
+    ScreenOrientation.addOrientationChangeListener(({ orientationInfo }) => {
+      console.log('orientationInfo.orientation', orientationInfo.orientation);
+      setOrientation(orientationInfo.orientation);
+    });
+
+    return () => {
+      ScreenOrientation.removeOrientationChangeListeners();
+      ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT_UP
+      );
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
